9aab415e8c88997f133154dbbf76e4bfbca8be4f
ElRaptorus <christian.werner@5minds.de>
2018-09-12 14:35:53 +0200

    :sparkles: Import from heatmap poc

---
+++ src/logging_api_service.ts
@@ -0,0 +1,50 @@
+import {ILoggingApiService, ILoggingRepository, LogEntry, LogLevel} from '@process-engine/logging_api_contracts';
+
+import {IIAMService, IIdentity} from '@essential-projects/iam_contracts';
+
+export class LoggingApiService implements ILoggingApiService {
+
+  private _iamService: IIAMService;
+  private _loggingRepository: ILoggingRepository;
+
+  constructor(iamService: IIAMService, loggingRepository: ILoggingRepository) {
+    this._iamService = iamService;
+    this._loggingRepository = loggingRepository;
+  }
+
+  private get iamService(): IIAMService {
+    return this._iamService;
+  }
+
+  private get loggingRepository(): ILoggingRepository {
+    return this._loggingRepository;
+  }
+
+  // TODO: Add claim checks as soon as necessary claims have been defined.
+  public async readLogForCorrelation(identity: IIdentity, correlationId: string): Promise<Array<LogEntry>> {
+    return this.loggingRepository.readLogForCorrelation(correlationId);
+  }
+
+  // TODO: Add claim checks as soon as necessary claims have been defined.
+  public async readLogForProcessModel(identity: IIdentity, correlationId: string, processModelId: string): Promise<Array<LogEntry>> {
+    return this.loggingRepository.readLogForProcessModel(correlationId, processModelId);
+  }
+
+  public async writeLogForProcessModel(correlationId: string,
+                                       processModelId: string,
+                                       logLevel: LogLevel,
+                                       message: string,
+                                       timestamp: Date): Promise<void> {
+    await this.loggingRepository.writeLogForProcessModel(correlationId, processModelId, logLevel, message, timestamp);
+  }
+
+  public async writeLogForFlowNode(correlationId: string,
+                                   processModelId: string,
+                                   flowNodeInstanceId: string,
+                                   flowNodeId: string,
+                                   logLevel: LogLevel,
+                                   message: string,
+                                   timestamp: Date): Promise<void> {
+    await this.loggingRepository.writeLogForFlowNode(correlationId, processModelId, flowNodeInstanceId, flowNodeId, logLevel, message, timestamp);
+  }
+}
